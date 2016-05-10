b541ec26777aaca3b398248caf0a04a969b1dd40
Andreas Lengauer <andreaslengauer94@gmail.com>
2016-05-11 00:04:48 +0200

    Erweitern der DTO Klassen um Stampcode darzustellen

---
+++ app/src/main/java/com/jku/stampit/dto/StampCodeDTO.java
@@ -0,0 +1,16 @@
+package com.jku.stampit.dto;
+
+/**
+ * Created by Andreas on 10.05.16.
+ */
+public class StampCodeDTO {
+    private String stampcode;
+
+    public String getStampcode() {
+        return stampcode;
+    }
+
+    public void setStampcode(String stampcode) {
+        this.stampcode = stampcode;
+    }
+}
